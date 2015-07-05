7082157055526a170a3ffa0fae290920a6904a83
David Nabraczky <david.nabraczky@gmail.com>
2015-07-05 08:08:27 +0200

    adding: load_file and load_config methods to irb

--- WindawesomeIrb/Program.cs
+++ WindawesomeIrb/Program.cs
@@ -4,6 +4,23 @@ using Microsoft.Scripting;
 
 namespace WindawesomeIrb
 {
+  public class Test
+  {
+    public string Name { get; set; }
+
+    public int Value { get; set; }
+
+    public Test()
+    {
+    }
+
+    public Test(string name, int value)
+    {
+      Name = name;
+      Value = value;
+    }
+  }
+
   public static class Irb
   {
     private static Engine _engine;
@@ -25,19 +42,32 @@ namespace WindawesomeIrb
       return _configPath;
     }
 
-    public static void ExecuteConfig(string configFilename)
+    public static void LoadConfig(string configFilename)
+    {
+      LoadFileWithErrorHandling(() =>
+        {
+          var fileInfo = new FileInfo(_configPath + configFilename);
+          _engine.Execute(fileInfo.FullName);
+        });
+    }
+
+    public static void LoadFile(string fullPath)
+    {
+      LoadFileWithErrorHandling(() => _engine.Execute(fullPath));
+    }
+
+    private static void LoadFileWithErrorHandling(Action action)
     {
       try
       {
-        var fileInfo = new FileInfo(_configPath + configFilename);
-        _engine.Execute(fileInfo.FullName);
+        action();
       }
-      catch(SyntaxErrorException e)
+      catch (SyntaxErrorException e)
       {
         Console.WriteLine("Error starting at line {0} column {1}", e.Line, e.Column);
         Console.WriteLine(e.Message);
       }
-      catch(Exception e)
+      catch (Exception e)
       {
         Console.WriteLine(e.Message);
       }
